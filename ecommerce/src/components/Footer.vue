cf8d7d87ca96657a7741da3229d3fc3f10769761
Thiago Tsuyoshi Okada Aoki <thiagookadaaoki@outlook.com>
2025-06-06 22:54:05 -0300

    Delete ecommerce directory

--- ecommerce/src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="bg-white mt-10 py-4 text-center text-sm text-gray-500">
-    © 2025 Loja AOCE. Todos os direitos reservados.
-  </footer>
-</template>
-
-<script setup>
-</script>
